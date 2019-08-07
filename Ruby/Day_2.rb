0a1f2951710761f828ad50dc0dbffc1736862cae
Rory Collins <rory.collins@codurance.com>
2019-08-07 09:23:35 +0100

    completes day 2

--- Ruby/Day_2.rb
+++ Ruby/Day_2.rb
@@ -8,3 +8,40 @@ array.each do |el|
 end
 
 #1b. Now do the same with each_slice in Enumerable
+
+(1..16).each_slice(4) { |a| p a }
+
+#Let the tree initializer accept a nested structure with hashes and arrays.
+
+class Tree
+	attr_accessor :children, :node_name
+	
+	def initialize(structure)
+		@node_name=structure.keys.first
+		@children=[]
+		structure[@node_name].each {|k, v| @children << Tree.new(k => v)}
+	end
+	
+	def visit_all(&block)
+		visit &block
+		children.each {|c| c.visit_all &block}
+	end
+	
+	def visit(&block)
+		block.call self
+	end
+end
+
+family_tree = Tree.new({'grandpa'=> {'dad'=> {'child 1' => {}, 'child 2' => {}}, 'uncle'=>{'child 3' => {}, 'child 4' => {}}}})
+family_tree.visit_all{|node| puts node.node_name}
+
+#Write a simple grep that will print the lines of a file having any occurrences of a 
+# phrase anywhere in that line
+
+def grep(pattern, filename)
+	File.open(filename) do |f|
+		f.each_line.with_index {|line, index| puts "#{index+1} #{line}" if line.match(/(#{pattern})/)}
+	end
+end
+
+grep "text", "text.txt"
